391c22744b8e156d55eccae29cfa0f5232daf7ad
Yoav <yoav.sh.j@gmail.com>
2022-07-28 10:06:11 +0300

    added

--- DataObfuscator.hpp
+++ DataObfuscator.hpp
@@ -1,5 +1,6 @@
 #include <initializer_list>
-		
+#include <type_traits>
+
 
 namespace DataObfuscator {
 
@@ -13,49 +14,90 @@ namespace DataObfuscator {
 	template<>
 	struct Make_Indexes<0> { using type = Indexes<>; };
 
+	namespace ArrayObfuscator {
+		template<typename T, T(*enc_fun)(T, int), T(*dec_fun)(T, int), typename Indexes> struct ArrayObfuscator;
 
-	template<typename T, T(*enc_fun)(T, int), T(*dec_fun)(T, int), typename Indexes> struct DataObfuscator;
+		template<typename T, T(*enc_fun)(T, int), T(*dec_fun)(T, int), int... I>
+		struct ArrayObfuscator<T, enc_fun, dec_fun, Indexes<I...>> {
 
-	template<typename T, T(*enc_fun)(T, int), T(*dec_fun)(T, int), int... I>
-	struct DataObfuscator<T, enc_fun, dec_fun, Indexes<I...>> {
+			constexpr __forceinline ArrayObfuscator(const std::initializer_list<T> list) : buffer{ encrypt(*(list.begin() + I), I) ... }, size{ sizeof...(I) }
+			{ }
 
-		constexpr __forceinline DataObfuscator(const std::initializer_list<T> list) : _buffer{ encrypt(*(list.begin() + I), I) ... } , size{ sizeof...(I) }
-		{ }
+			inline void decrypt()
+			{
+				for (size_t i = 0; i < sizeof...(I); i++)
+					buffer[sizeof...(I) - i - 1] = decrypt(buffer[sizeof...(I) - i - 1], sizeof...(I) - i - 1);
 
-		inline const void decrypt()
+			}
+
+			T& operator[](int i)
+			{
+				return buffer[i];
+			}
+
+			int getsize()
+			{
+				return size;
+			}
+
+
+		private:
+			// compile time encryption / decryption routines.
+			constexpr T __forceinline encrypt(T c, int i) const { return enc_fun(c, i); }
+
+			T decrypt(T c, int i) { return dec_fun(c, i); }
+
+			T buffer[sizeof...(I)];
+			const int size;
+		};
+
+		constexpr unsigned int list_size(const std::initializer_list<int> list)
 		{
-			for (size_t i = 0; i < sizeof...(I); i++)
-				_buffer[i] = decrypt(_buffer[i], i);
+			return list.size();
 		}
+	}
 
-		T& operator[](int i) 
+
+	namespace StringObfuscator {
+		// Represent an obfuscated string, recieves type (char / wchar_t), encryption function, decryption function and Index array.
+		template<typename T, T(*enc_fun)(T, int), T(*dec_fun)(T, int), typename Indexes>
+		struct StringObfuscator;
+
+
+		template<typename T, T(*enc_fun)(T, int), T(*dec_fun)(T, int), int... I>
+		struct StringObfuscator<T, enc_fun, dec_fun, Indexes<I...>>
 		{
-			return _buffer[i];
-		}
+			// Constructor evaluated at compile time - encrypting string.
+			constexpr __forceinline StringObfuscator(const T* str) : buffer_{ encrypt(str[I], I)... }
+			{ }
 
-		int getsize()
-		{
-			return size;
-		}
+			// Decrypting the string at runtime.
+			inline const T* decrypt()
+			{
+				for (size_t i = 0; i < sizeof...(I); ++i)
+					buffer_[sizeof...(I) - i - 1] = decrypt(buffer_[sizeof...(I) - i - 1], sizeof...(I) - i - 1);
 
-		
+				buffer_[sizeof...(I)] = 0;
 
-	private:
-		// compile time encryption / decryption routines.
-		constexpr T __forceinline encrypt(T c, int i) const { return enc_fun(c, i); }
-		constexpr T decrypt(T c, int i) const { return dec_fun(c, i); }
+				return const_cast<const T*>(buffer_);
+			}
 
-		T _buffer[sizeof...(I)];
-		const int size;
-	};
+		private:
 
-	constexpr unsigned int list_size(const std::initializer_list<int> list)
-	{
-		return list.size();
+			// compile time encryption / decryption routines.
+			constexpr T __forceinline encrypt(T c, int i) const { return enc_fun(c, i); }
+			constexpr T decrypt(T c, int i) const { return dec_fun(c, i); }
+
+			// Buffer to store the encrypted string + terminating null byte
+			volatile T buffer_[sizeof...(I) + 1];
+		};
 	}
 }
 
-#define SINGLE_ARG(...) __VA_ARGS__
+#define DATAOBJ_SINGLE_ARG(...) __VA_ARGS__
+#define DATAOBJ_OBFDATA(t, var_name, enc_fun, dec_fun, ...) DataObfuscator::ArrayObfuscator::ArrayObfuscator<t, enc_fun, dec_fun, DataObfuscator::Make_Indexes<DataObfuscator::ArrayObfuscator::list_size(__VA_ARGS__)>::type> var_name(__VA_ARGS__);// var_name.decrypt();
 
-#define OBF_GET_OVERALL_TYPE(t, enc_fun, dec_fun, ...) DataObfuscator::DataObfuscator<t, enc_fun, dec_fun, DataObfuscator::Make_Indexes<DataObfuscator::list_size(__VA_ARGS__)>::type>
-#define OBF_DATA(t, var_name, enc_fun, dec_fun, ...) OBF_GET_OVERALL_TYPE(t, enc_fun, dec_fun, __VA_ARGS__) var_name(__VA_ARGS__); var_name.decrypt();
+#define DATAOBJ_GET_STR_TYPE(str) std::remove_const<std::remove_pointer<std::decay<decltype(str)>::type>::type>::type
+#define DATAOBJ_OBFSTR(str, enc_func, dec_func, ...) (DataObfuscator::StringObfuscator::StringObfuscator<DATAOBJ_GET_STR_TYPE(str), enc_func<DATAOBJ_GET_STR_TYPE(str), ##__VA_ARGS__>, dec_func<DATAOBJ_GET_STR_TYPE(str), ##__VA_ARGS__>, DataObfuscator::Make_Indexes<sizeof(str) - 1>::type>(str)).decrypt()
+
+#define DATAOBJ_FUNC(funcname, ...) template<typename T, ##__VA_ARGS__> constexpr T __forceinline funcname(T c, int i)
--- example.cpp
+++ example.cpp
@@ -1,15 +1,42 @@
 #include "DataObfuscator.hpp"
 #include <stdio.h>
 
-template<typename T, int K> constexpr T __forceinline e(T c, int i) { return c + K; }
-template<typename T, int K> constexpr T __forceinline d(T c, int i) { return c - K; }
+template<typename T, int K> constexpr T __forceinline e1(T c, int i) { return c + K; }
+template<typename T, int K> T __forceinline d1(T c, int i) { return c - K; }
+
+template<typename T, unsigned int K1, unsigned int K2, unsigned int K3, unsigned int K4> constexpr T __forceinline e2(T c, int i) { return ((((c + K1) ^ K2) - K3) ^ K4); }
+template<typename T, unsigned int K1, unsigned int K2, unsigned int K3, unsigned int K4> constexpr T __forceinline d2(T c, int i) { return ((((c ^ K4) + K3) ^ K2) - K1); }
+
+DATAOBJ_FUNC(e3, unsigned int K1, unsigned int K2, unsigned int K3, unsigned int K4)
+{
+	return ((((c + K1) ^ K2) - K3) ^ K4);
+}
+
+DATAOBJ_FUNC(d3, unsigned int K1, unsigned int K2, unsigned int K3, unsigned int K4)
+{
+	return ((((c ^ K4) + K3) ^ K2) - K1);
+}
 
 int main()
 {
-	OBF_DATA(long long, da, SINGLE_ARG(e<long long, 1>), SINGLE_ARG(d<long long, 1>), {0,1,2,3});
-
-	for (size_t i = 0; i < da.getsize(); i++)
+	DATAOBJ_OBFDATA(long long, data1, DATAOBJ_SINGLE_ARG(e1<long long, 1>), DATAOBJ_SINGLE_ARG(d1<long long, 1>), {0,1,2,3});
+	for (size_t i = 0; i < data1.getsize(); i++)
 	{
-		printf("%d\n", da[i]);
+		printf("Obfuscated var at %d : %d\n", i, data1[i]);
 	}
+
+	DATAOBJ_OBFDATA(unsigned int, data2, DATAOBJ_SINGLE_ARG(e3<unsigned int, 0xFF, 0xEEEE, 0x12, 0x333333>), DATAOBJ_SINGLE_ARG(d3<unsigned int, 0xFF, 0xEEEE, 0x12, 0x333333>), {0x11111111, 0x33333333, 0x33333333, 0x77777777});
+	for (size_t i = 0; i < data2.getsize(); i++)
+	{
+		printf("Obfuscated var at %d : %x\n", i, data2[i]);
+	}
+
+	// OBF(str, e, d, K, W)
+	printf(DATAOBJ_OBFSTR("This is an encrypted string 1\n", e1, d1, 0x01));
+
+	printf(DATAOBJ_OBFSTR("This is an encrypted string 2\n", e2, d2, 0x11, 0x33, 0x33, 0x77));
+
+	wprintf(DATAOBJ_OBFSTR(L"This is an encrypted string 3\n", e3, d3, 0x11, 0x33, 0x33, 0x77));
+
+
 }
